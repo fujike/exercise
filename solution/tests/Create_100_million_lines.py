f7ba3fb5fa3c936692917073e6f2b3c3a134a477
fujike <kei_fujihira@kk-grp.jp>
2019-08-26 17:46:09 +0900

    Add files via upload

---
+++ solution/tests/Create_100_million_lines.py
@@ -0,0 +1,26 @@
+# coding: shift-JIS
+
+import time
+import os
+import sys
+import random
+
+t1 = time.time()
+
+print('\n Start ... \n')
+
+os.chdir(sys.path[0])
+
+outfile = 'line_100_million.txt'
+
+out = open(outfile, 'w')
+
+for i in range(1, 100000000 + 1):
+  id = str(i).zfill(9)
+  v = random.randint(1, 999)
+  out.write('%s %d\n' % (id, v))
+
+out.close()
+
+print('\n ˆ—ŠÔ: %.1f •b' % (time.time() - t1))
+print('\n End. \n')
